0f4e219e4e81dad549904320e8876437505f4a23
/usr/bin/vi <support@beget.ru>
2020-02-20 19:20:08 +0300

    Add default mudule template

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace vendor\klunker\pages;
+
+/**
+ * pages module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'vendor\klunker\pages\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
